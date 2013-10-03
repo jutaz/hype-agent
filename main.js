eb196368eb7c284d6b32ab73effb5d6452c2f962
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-03 15:45:14 +0300

    add unzip.

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 var pushover = require('pushover');
 var path = require('path');
 var helper = require('./lib/helper');
+var unzip = require('unzip');
+var fs = require('fs');
 var conf = require("./conf.json");
 var repos = pushover(path.normalize(conf.dir+'/repos'));
 
@@ -8,6 +10,7 @@ repos.on('push', function (push) {
 	console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
 	push.accept();
 	helper.clone_to_dir(push, function(file, push) {
+		fs.createReadStream(file).pipe(unzip.Extract({ path: conf.dir }));
 		console.log('clone ' + file + '(' + push.branch + ')');
 	});
 });
