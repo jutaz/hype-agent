a8f3f3de961229f7f1f46de37e6a9c80b2a00a29
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-03 18:58:13 +0300

    Will definitely use git-server

--- main.js
+++ main.js
@@ -4,45 +4,26 @@ var helper = require('./lib/helper');
 var unzip = require('unzip');
 var fs = require('fs');
 var conf = require("./conf.json");
-var repos = pushover(path.normalize(conf.dir+'/repos'));
 
 var newUser = {
 	username:'demo',
 	password:'demo'
 }
 var newRepo = {
-	name:'test',
+	name:'gitter',
 	anonRead:false,
 	users: [ { user:newUser, permissions:['R','W'] } ],
 	onSuccessful : {
 		fetch : function( repo, method ){
 			console.log('Successful fetch/pull/clone on repo:',repo.name);
-		}
+		},
 		push  : function( repo, method ){
-			console.log('PUSHed:', repo.name);
-            // Possibly do some deploy scripts etc.
-        }
-    }
+			console.log('PUSHed:', repo, method);
+			helper.clone_to_dir(repo, function(file, push) {
+				fs.createReadStream(file).pipe(unzip.Extract({ path: conf.dir }));
+				console.log('clone ' + file + '(' + push.branch + ')');
+			});
+		}
+	}
 }
-_g = new GitServer([ newRepo ], true, conf.dir);
-
-// repos.on('push', function (push) {
-// 	console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
-// 	push.accept();
-// 	helper.clone_to_dir(push, function(file, push) {
-// 		fs.createReadStream(file).pipe(unzip.Extract({ path: conf.dir }));
-// 		console.log('clone ' + file + '(' + push.branch + ')');
-// 	});
-// });
-
-// repos.on('fetch', function (fetch) {
-// 	console.log('fetch ' + fetch.commit);
-// 	fetch.accept();
-// });
-
-// var http = require('http');
-// var server = http.createServer(function (req, res) {
-// 	repos.handle(req, res);
-// });
-
-// server.listen(7000);
+_g = new GitServer([ newRepo ], true, path.normalize(conf.dir+'/repos'));
