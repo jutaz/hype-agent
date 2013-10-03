40510594a610750aa0e1737f83b14c3b8dff4bc1
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-03 16:08:34 +0300

    testing git-server....

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var pushover = require('pushover');
+var GitServer = require('git-server');
 var path = require('path');
 var helper = require('./lib/helper');
 var unzip = require('unzip');
@@ -6,23 +6,43 @@ var fs = require('fs');
 var conf = require("./conf.json");
 var repos = pushover(path.normalize(conf.dir+'/repos'));
 
-repos.on('push', function (push) {
-	console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
-	push.accept();
-	helper.clone_to_dir(push, function(file, push) {
-		fs.createReadStream(file).pipe(unzip.Extract({ path: conf.dir }));
-		console.log('clone ' + file + '(' + push.branch + ')');
-	});
-});
+var newUser = {
+	username:'demo',
+	password:'demo'
+}
+var newRepo = {
+	name:'test',
+	anonRead:false,
+	users: [ { user:newUser, permissions:['R','W'] } ],
+	onSuccessful : {
+		fetch : function( repo, method ){
+			console.log('Successful fetch/pull/clone on repo:',repo.name);
+		}
+		push  : function( repo, method ){
+			console.log('PUSHed:', repo.name);
+            // Possibly do some deploy scripts etc.
+        }
+    }
+}
+_g = new GitServer([ newRepo ], true, conf.dir);
 
-repos.on('fetch', function (fetch) {
-	console.log('fetch ' + fetch.commit);
-	fetch.accept();
-});
+// repos.on('push', function (push) {
+// 	console.log('push ' + push.repo + '/' + push.commit + ' (' + push.branch + ')');
+// 	push.accept();
+// 	helper.clone_to_dir(push, function(file, push) {
+// 		fs.createReadStream(file).pipe(unzip.Extract({ path: conf.dir }));
+// 		console.log('clone ' + file + '(' + push.branch + ')');
+// 	});
+// });
 
-var http = require('http');
-var server = http.createServer(function (req, res) {
-	repos.handle(req, res);
-});
+// repos.on('fetch', function (fetch) {
+// 	console.log('fetch ' + fetch.commit);
+// 	fetch.accept();
+// });
 
-server.listen(7000);
+// var http = require('http');
+// var server = http.createServer(function (req, res) {
+// 	repos.handle(req, res);
+// });
+
+// server.listen(7000);
