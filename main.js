4f9693079c6122ba363e9858bc9e3a90746ec564
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-04 20:11:39 +0300

    this now works with mongoDB!

--- main.js
+++ main.js
@@ -5,13 +5,17 @@ var unzip = require('unzip');
 var fs = require('fs');
 var conf = require("./conf.json");
 var db = require('./lib/db');
-var repos = {};
+var repos = [];
 
 db.collection('repos').find().toArray(function(err, repos) {
 	repos.forEach(function(item) {
-		db.collection('users').find({"repo": item.name}).toArray(function(err, users) {
-			item.users = users;
-			item.onSuccessful: {
+		db.collection('users').find({"repos": { $in: [item.name] }}).toArray(function(err, users) {
+			tmpUsers = [];
+			users.forEach(function(user) {
+				tmpUsers.push({user: {username: user.username, password: user.password}, permissions: user.repo_perms[item.name]});
+			});
+			item.users = tmpUsers;
+			item.onSuccessful = {
 				fetch: helper.fetch,
 				push: helper.push
 			}
@@ -19,5 +23,4 @@ db.collection('repos').find().toArray(function(err, repos) {
 		});
 	});
 });
-
-server = new GitServer([ newRepo, newRepo2 ], true, path.normalize(conf.dir+'/repos'));
+server = new GitServer(repos, true, path.normalize(conf.dir+'/repos'));
