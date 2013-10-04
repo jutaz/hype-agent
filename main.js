7ccefcc06a1cf466510817194ef191f1e2e2a4e6
Justas Brazauskas <brazauskasjustas@gmail.com>
2013-10-04 18:41:39 +0300

    its better to stick with git clone instead of using zip archives.

--- main.js
+++ main.js
@@ -34,4 +34,29 @@ var newRepo = {
 		}
 	}
 }
-server = new GitServer([ newRepo ], true, path.normalize(conf.dir+'/repos'));
+
+var newRepo2 = {
+	name:'kloxo',
+	anonRead:false,
+	users: [ { user:newUser, permissions:['R','W'] } ],
+	onSuccessful: {
+		fetch: function(repo, method, push) {
+			console.log('Successful fetch/pull/clone on repo:',repo.name);
+		},
+		push: function(repo, method, push) {
+			console.log('PUSHed:', repo, method);
+			em(push.cwd).on('post-update', function(update) {
+				var timeStart = new Date().getTime();
+				update.name = push.name;
+				update.commit = push.commit;
+				update.branch = push.branch;
+				update.cwd = push.cwd;
+				helper.clone_to_dir(update, function(file, push) {
+					console.log('clone ' + file + '(' + push.branch + ')', "time:", (new Date().getTime()-timeStart));
+				});
+			});
+		}
+	}
+}
+
+server = new GitServer([ newRepo, newRepo2 ], true, path.normalize(conf.dir+'/repos'));
